72a0072d886c9417a5688970eff35bc8d355499f
StijnVanRiet <stijn.v.riet@gmail.com>
2026-03-13 11:25:50 +0100

    cleanup upload file

--- app/ingest.py
+++ app/ingest.py
@@ -4,18 +4,21 @@ from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores.pgvector import PGVector
 from pathlib import Path
 import re
+import os
 
 from app.config import DB_CONNECTION, COLLECTION_NAME
 
 
 def clean_text(text: str) -> str:
-    if text:
-        text = text.replace("\x00", "")
-        text = re.sub(r"\s+", " ", text)
-        text = text.strip()
+    """
+    Clean problematic characters from PDF text.
+    """
+    text = text.replace("\x00", "")  # remove NUL characters
+    text = re.sub(r"\s+", " ", text)  # normalize whitespace
+    text = text.strip()
     return text
 
-
+# TODO: duplicate code
 def load_documents():
     docs = []
     for file in Path(COLLECTION_NAME).glob("*.pdf"):
@@ -26,10 +29,11 @@ def load_documents():
         pdf_docs = loader.load()
 
         for doc in pdf_docs:
-            # remove NUL characters
             doc.page_content = clean_text(doc.page_content)
             # set source metadata of each page to filename
             doc.metadata["source"] = file.name
+            # page number
+            doc.metadata["page"] = doc.metadata.get("page", 0)
 
         docs.extend(pdf_docs)
 
@@ -61,11 +65,18 @@ def ingest():
 
 def ingest_file(path):
     loader = PyPDFLoader(path)
-    docs = loader.load()
+    pdf_docs = loader.load()
+    filename = os.path.basename(path)
+    for doc in pdf_docs:
+        doc.page_content = clean_text(doc.page_content)
+        # set source metadata of each page to filename
+        doc.metadata["source"] = filename
+        # page number
+        doc.metadata["page"] = doc.metadata.get("page", 0)
 
     splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 
-    split_docs = splitter.split_documents(docs)
+    split_docs = splitter.split_documents(pdf_docs)
 
     embeddings = OllamaEmbeddings(model="nomic-embed-text")
 
@@ -75,6 +86,8 @@ def ingest_file(path):
         collection_name=COLLECTION_NAME,
         connection_string=DB_CONNECTION,
     )
+
+    print(f"{filename} ingested successfully")
 
 
 if __name__ == "__main__":
